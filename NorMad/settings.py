ede7b72b847b9f4b92c3da9301fb0b4b186ec1dc
abrarbinrofique <abrar46@student.sust.edu>
2024-09-22 18:57:04 +0600

    update env part

--- NorMad/settings.py
+++ NorMad/settings.py
@@ -131,13 +131,11 @@ AUTH_USER_MODEL = 'customer.Traveller'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_PATH', 'db.sqlite3'),
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
