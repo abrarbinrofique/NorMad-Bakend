ce4e3202dcd9d6f23d8f5f84c63a37b6d5ea34bb
abrarbinrofique <abrar46@student.sust.edu>
2024-09-24 19:29:29 +0600

    updates

--- NorMad/settings.py
+++ NorMad/settings.py
@@ -20,7 +20,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
@@ -126,10 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -139,14 +135,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR / 'staticfiles'
-
-# Specify the directory where media files are stored
-# MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 
